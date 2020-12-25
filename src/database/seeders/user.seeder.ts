2778883e654a0f7a6d8114d70dfa7f2aa4b45c22
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-12-25 10:13:23 +0100

    Create helpers/sleep.ts

--- src/database/seeders/user.seeder.ts
+++ src/database/seeders/user.seeder.ts
@@ -2,6 +2,7 @@ import { UsersService } from '../../routes/user/users.service';
 import faker from 'faker';
 import { RegisterRequestDTO } from '../../routes/auth/dto/register.dto';
 import { hashString } from '../../common/helpers/hash-string';
+import { logger } from '../../common/utils/logger';
 
 export class UserSeeder {
     private _fakeUserData!: RegisterRequestDTO;
@@ -24,5 +25,21 @@ export class UserSeeder {
     private async _createFakeDataWithHashedPassword(): Promise<void> {
         const hashedPassword = await hashString(this._fakeUserData.password);
         this._fakeUserDataWithHashedPassword = { ...this._fakeUserData, password: hashedPassword };
+    }
+
+    private async _saveUserAccount(): Promise<void> {
+        try {
+            await this._usersService.create(this._fakeUserDataWithHashedPassword);
+        } catch(error) {
+            logger.red(error.message);
+        }
+    }
+
+    private async _printUserCredentialsAfterSleep(): Promise<void> {
+        logger.green('User generated successfully');
+
+        await sleep(1500);
+
+        console.log(this._fakeUserData);
     }
 }

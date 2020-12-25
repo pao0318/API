f880725fae36cbafecd05c549201a8a02e8e2e68
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-12-25 20:17:57 +0100

    App second and third test case to common/__tests__/config-validator.test.ts

--- src/common/__tests__/config-validator.test.ts
+++ src/common/__tests__/config-validator.test.ts
@@ -5,23 +5,61 @@ import faker from 'faker';
 import { Constants } from '../constants';
 
 describe('Config validator - validate method', () => {
-    const config = {} as typeof globalConfig;
+    const config = {
+        APP: {
+            MODE: Constants.AppMode.TEST,
+            PREFIX: 'api/v1/',
+            PORT: 4000
+        },
+        DATABASE: {
+            NAME: "main",
+            URL: "mongodb://"
+        }
+    } as typeof globalConfig;
 
-    describe('When APP.MODE is invalid', () => {
-        beforeAll(() => {
-            config.APP.MODE = faker.random.alphaNumeric();
-        });
-        
-        it('Should call process.exit with code 1', async () => {
+    describe('When all variables are valid', () => {
+        it('Should not call process.exit', async () => {
             const mockedExit = jest.spyOn(process, 'exit').mockImplementation(number => number as never);
 
             await ConfigValidator.validate(config);
-
-            expect(mockedExit).toHaveBeenCalledWith(1);
+        
+            expect(mockedExit).not.toHaveBeenCalled();
+        });
+    });
+    
+    describe('When APP.MODE is invalid', () => {
+        beforeAll(() => {
+            config.APP.MODE = faker.random.word();
+        });
+        
+        it('Should call process.exit with code 1', async () => {
+            await checkConfig(config);
         });
 
         afterAll(() => {
             config.APP.MODE = Constants.AppMode.TEST
         });
     });
+
+    describe('When APP.PREFIX is invalid', () => {
+        beforeAll(() => {
+            config.APP.PREFIX = faker.random.number()
+        });
+        
+        it('Should call process.exit with code 1', async () => {
+            await checkConfig(config);
+        });
+
+        afterAll(() => {
+            config.APP.PREFIX = '/api/v1';
+        });
+    });
 }); 
+
+async function checkConfig(config: typeof globalConfig): Promise<void> {
+    const mockedExit = jest.spyOn(process, 'exit').mockImplementation(number => number as never);
+
+    await ConfigValidator.validate(config);
+
+    expect(mockedExit).toHaveBeenCalledWith(1);
+}

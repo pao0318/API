3f3afccd3c2ce8030e12cbca2bd4235fd4ce76d5
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-12-28 09:29:51 +0100

    Create config/auth.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,9 @@
 import appConfig from './app';
+import authConfig from './auth';
 import databaseConfig from './database';
 
 export default {
     APP: appConfig,
+    AUTH: authConfig,
     DATABASE: databaseConfig
 } as const;

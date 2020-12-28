cb46d2b942583b827ce51897fff56e9e794665eb
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-12-28 09:22:16 +0100

    Create types/index.d.ts

--- src/services/jwt/jwt.service.ts
+++ src/services/jwt/jwt.service.ts
@@ -1,6 +1,10 @@
 import Token from '../../common/constants/token';
+import { TokenPayload } from '../../types';
 import { IAccessTokenPayload } from './interfaces/IAccessTokenPayload';
 
 export class JwtService {
     public generateToken(type: Token.ACCESS, payload: IAccessTokenPayload): string
+    public generateToken(type: Token, payload: TokenPayload): string {
+        
+    }
 }

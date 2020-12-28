5588336c9e5fc7fd9f1ba86e8a0a1c11e344e81b
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-12-28 09:32:24 +0100

    Update config-validator.test.ts

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config'
 
 export default {
-    ACCESS_TOKEN_SECRET: process.env.AUTH_ACCESS_TOKEN_SECRET
+    ACCESS_TOKEN_SECRET: process.env.AUTH_ACCESS_TOKEN_SECRET!
 } as const;

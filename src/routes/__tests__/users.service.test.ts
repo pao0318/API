7a2f234c0859f229e30bdef12fb5c3a567867feb
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-12-28 19:57:27 +0100

    Add first test case to routes/__tests__/users.service.test.ts

---
+++ src/routes/__tests__/users.service.test.ts
@@ -0,0 +1,53 @@
+import { TestUtils } from '../../common/utils/test-utils';
+import { UsersService } from '../user/users.service';
+import faker from 'faker';
+
+beforeAll(async () => {
+    await TestUtils.connectToDatabase();
+});
+
+afterAll(async () => {
+    await TestUtils.dropDatabase();
+});
+
+describe('Users service', () => {
+    const usersService = new UsersService();
+
+    describe('Get many method', () => {
+        describe('When users do not exist', () => {
+            it('Should return empty array', async () => {
+                const users = await usersService.getMany();
+                expect(users).toHaveLength(0);
+            });
+        });
+
+        describe('When users exist but arguments do not match any', () => {
+            beforeAll(async () => {
+                await usersService.create({ email: faker.internet.email(), name: faker.internet.userName(), password: faker.internet.password() });
+            });
+
+            it('Should return empty array', async () => {
+                const users = await usersService.getMany({ email: faker.internet.email() });
+                expect(users).toHaveLength(0);
+            });
+        });
+
+        describe('When more than one user exists and provided arguments matches a record', () => {
+            const userData = { email: faker.internet.email(), name: faker.internet.userName(), password: faker.internet.password() };
+            
+            beforeAll(async () => {
+                await usersService.create(userData);
+            });
+
+            it('Should return one record', async () => {
+                const users = await usersService.getMany({ email: userData.email });
+                expect(users).toHaveLength(1);
+            });
+
+            it('Should return user that matches provided data', async () => {
+                const users = await usersService.getMany({ email: userData.email });
+                expect(users[0]).toMatchObject(userData);
+            });
+        });
+    });
+});

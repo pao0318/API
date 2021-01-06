e8129573b5a032c789e09fb4c0d1c3ef86751ea7
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-06 09:46:34 +0100

    Create database/models/user/interfaces/IUserProvider.ts

--- src/database/models/user/interfaces/IUserProvider.ts
+++ src/database/models/user/interfaces/IUserProvider.ts
@@ -1,5 +1,7 @@
 import { CreateUserDTO } from '../dto/create.dto';
+import { DeleteUserDTO } from '../dto/delete.dto';
 import { GetUserDTO } from '../dto/get.dto';
+import { UpdateUserDTO } from '../dto/update.dto';
 import { IUser } from './IUser';
 
 export interface IUserProvider {
@@ -9,5 +11,7 @@ export interface IUserProvider {
 
     create(data: CreateUserDTO): Promise<IUser>;
 
-    deleteOne(data: )
+    deleteOne(data: DeleteUserDTO): Promise<void>;
+
+    findByIdAndUpdate(id: string, data: UpdateUserDTO): Promise<IUser | null>
 }

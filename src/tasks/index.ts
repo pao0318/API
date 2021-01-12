d06ef13159584ff77e462ef4c477b8ddb60b609b
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-12 10:22:42 +0100

    Fix lint issues

---
+++ src/tasks/index.ts
@@ -0,0 +1,11 @@
+// import config from '../config';
+// import { ITasksProvider } from './interfaces/ITasksProvider';
+// import { AgendaProvider } from './providers/agenda.provider';
+
+// export class TasksManager {
+//     constructor(private readonly _tasksProvider: ITasksProvider = new AgendaProvider(config.DATABASE.URL)) {}
+
+//     public async init(): Promise<void> {
+//         console.log()
+//     }
+// }

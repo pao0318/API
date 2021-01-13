621f975b9c6ccb53597c7a6786a957c302d708ef
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-13 11:28:11 +0100

    Create helpers/generate-confirmation-code.ts

---
+++ src/common/helpers/generate-confirmation-code.ts
@@ -0,0 +1,10 @@
+import { randomBytes } from 'crypto';
+import { IConfirmationCode } from '../../models/user/interfaces/IConfirmationCode';
+import { Constants } from '../constants';
+
+export function generateConfirmationCode(): IConfirmationCode {
+    return {
+        code: randomBytes(20).toString('hex').substr(8, 6),
+        expiresAt: Date.now() + Constants.TIME.MINUTES_30
+    };
+}

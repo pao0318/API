d98c7de21d4161b9de43ef4199c5d37423a845a0
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-14 21:42:12 +0100

    Create services/token/services/jwt.service.ts

--- src/services/token/interfaces/ITokenService.ts
+++ src/services/token/interfaces/ITokenService.ts
@@ -1,8 +1,11 @@
+import { AccessToken } from '../tokens/access-token';
+import { IAccessTokenPayload } from './IAccessTokenPayload';
 import { IToken } from './IToken';
 import { ITokenPayload } from './ITokenPayload';
 
 export interface ITokenService {
     generate(token: IToken): Promise<string>;
-    
-    verify(token: IToken, stringifyToken: string): Promise<ITokenPayload>
+
+    verify(token: AccessToken, stringifyToken: string): Promise<IAccessTokenPayload>;
+    verify(token: IToken, stringifyToken: string): Promise<ITokenPayload>;
 }
---
+++ src/services/token/services/jwt.service.ts
@@ -0,0 +1,21 @@
+import 'reflect-metadata';
+import { injectable } from 'inversify';
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+import { ITokenService } from '../interfaces/ITokenService';
+import { IToken } from '../interfaces/IToken';
+import { ITokenPayload } from '../interfaces/ITokenPayload';
+
+@injectable()
+export class JwtService implements ITokenService {
+    private readonly _verifyAsync = promisify<string, string, object | undefined>(jwt.verify);
+
+    public async generate(token: IToken): Promise<string> {
+        return jwt.sign(token.payload, token.secret, { expiresIn: token.expiresIn });
+    }
+
+    public async verify(token: IToken, stringifyToken: string): Promise<ITokenPayload> {
+        const payload = await this._verifyAsync(stringifyToken, token.secret);
+        return payload as ITokenPayload;
+    }
+}

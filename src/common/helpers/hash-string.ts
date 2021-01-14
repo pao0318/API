b475d2191517e99f54c69502019990cfc151c397
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-14 10:51:12 +0100

    Create exceptions/unconfirmed-account.exception.ts

--- src/common/helpers/hash-string.ts
+++ src/common/helpers/hash-string.ts
@@ -1,8 +1,6 @@
 import { hash } from 'bcrypt';
 
 export async function hashString(string: string): Promise<string> {
-    if(string.length === 0) throw new Error('String should be at least 1 characters long');
-
     const hashedString = await hash(string, 12);
     return hashedString;
 }

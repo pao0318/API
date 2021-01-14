24328708f64b8b171c3f08f87e229d5a8284320f
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-14 10:09:05 +0100

    Create routes/auth/schemes/login.schema.ts

---
+++ src/routes/auth/schemes/login.schema.ts
@@ -0,0 +1,6 @@
+import { object, string } from 'joi';
+
+export const LoginValidationSchema = object({
+    email: string().min(3).max(64).email().required(),
+    password: string().min(3).max(64).required()
+});

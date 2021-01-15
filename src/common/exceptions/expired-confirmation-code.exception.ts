cb20eb6500e9b55499cb6ffce5041a9f27598748
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-15 21:46:43 +0100

    Create exceptions/expired-confirmation-code.exception.ts

---
+++ src/common/exceptions/expired-confirmation-code.exception.ts
@@ -0,0 +1,8 @@
+import { Constants } from "../constants";
+import { BaseException } from "./base.exception";
+
+export class ExpiredConfirmationCodeException extends BaseException {
+    id = Constants.EXCEPTION.EXPIRED_CONFIRMATION_CODE;
+    statusCode = Constants.STATUS_CODE.FORBIDDEN;
+    message = 'Provided confirmation code has already expired'
+}

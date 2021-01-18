a317e28eb1d1d6a8959e92d3fda987d2332e92a2
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-18 15:50:37 +0100

    Create exceptions/user-not-found-exception.ts

---
+++ src/common/exceptions/user-not-found-exception.ts
@@ -0,0 +1,8 @@
+import { Constants } from "../constants";
+import { BaseException } from "./base.exception";
+
+export class UserNotFoundException extends BaseException {
+    id = Constants.EXCEPTION.USER_NOT_FOUND;
+    statusCode = Constants.STATUS_CODE.NOT_FOUND;
+    message = 'User does not exist'
+}

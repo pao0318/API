17321293bc9f5fdc1923b53b6d1d149737738cfd
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-21 19:48:16 +0100

    Create database/models/book/schemas/mongo.schema.ts

---
+++ src/database/models/book/schemas/mongo.schema.ts
@@ -0,0 +1,41 @@
+import { model, Schema } from 'mongoose';
+import { Constants } from '../../../../common/constants';
+import { IBookDocument } from '../interfaces/IBookDocument';
+
+export const MongoBookSchema = new Schema({
+    ownedBy: {
+        type: String,
+        required: true
+    },
+    borrowedBy: {
+        type: String,
+        default: null
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    genre: {
+        type: String,
+        required: true,
+        enum: [
+            Constants.BOOK.GENRE.ADVENTURE, Constants.BOOK.GENRE.DETECTIVE,
+            Constants.BOOK.GENRE.FANTASY, Constants.BOOK.GENRE.HORROR,
+            Constants.BOOK.GENRE.SCIENCE_FICTION, Constants.BOOK.GENRE.THRILLER
+        ]
+    },
+    picture: {
+        type: String,
+        required: true
+    },
+    addedAt: {
+        type: Number,
+        default: Date.now()
+    }
+});
+
+export const MongoBookModel = model<IBookDocument>('Book', MongoBookSchema);

17d0d92632fb87ac2aaf92d510ccd12fad9a5dee
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-28 10:30:20 +0100

    Add first test case to e2e/register.test.ts

--- src/e2e/register.test.ts
+++ src/e2e/register.test.ts
@@ -1,4 +1,5 @@
 import * as request from 'supertest';
+import { Response } from 'supertest';
 import { Test } from '@nestjs/testing';
 import { TestUtils } from '../common/utils/test-utils';
 import { AuthModule } from '../routes/auth/auth.module';
@@ -6,6 +7,7 @@ import { INestApplication } from '@nestjs/common';
 import { Constants } from '../common/constants';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Config } from '../common/config';
+import { ExceptionFilter } from '../common/filters/exception.filter';
 
 describe(`POST ${Constants.ENDPOINT.AUTH.REGISTER}`, () => {
     let app: INestApplication;
@@ -19,15 +21,36 @@ describe(`POST ${Constants.ENDPOINT.AUTH.REGISTER}`, () => {
         }).compile();
 
         app = module.createNestApplication();
-        await app.init();
-    });
 
-    it('Should be true', () => {
-        expect(true).toBeTruthy();
+        app.useGlobalFilters(new ExceptionFilter());
+
+        await app.init();
     });
 
     afterAll(async () => {
         await TestUtils.dropDatabase();
         await app.close();
+    });
+
+    describe('When input is invalid', () => {
+        let response: Response;
+
+        beforeAll(async (done) => {
+            response = await request(app.getHttpServer()).post(
+                Constants.ENDPOINT.AUTH.REGISTER,
+            );
+
+            done();
+        });
+
+        it('Should return status code 400', () => {
+            expect(response.status).toEqual(400);
+        });
+
+        it(`Should return error id ${Constants.EXCEPTION.INVALID_INPUT}`, () => {
+            expect(response.body.error.id).toEqual(
+                Constants.EXCEPTION.INVALID_INPUT,
+            );
+        });
     });
 });

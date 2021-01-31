e78dfb654d0862f9868e86b266ad7fed4b3d0ab7
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-31 21:44:35 +0100

    Rename reset-password-confirmation to password-reset

--- src/common/constants/endpoint.ts
+++ src/common/constants/endpoint.ts
@@ -1,18 +1,25 @@
 export const Endpoint = {
     AUTH: {
         REGISTER: '/auth/register',
-        LOGIN: '/auth/login',
-        LOGIN_GOOGLE: '/auth/login-google',
-        LOGIN_FACEBOOK: '/auth/login-facebook',
+        LOGIN: '/auth/email',
+        LOGIN_GOOGLE: '/auth/google',
+        LOGIN_FACEBOOK: '/auth/facebook',
         LOGOUT: '/auth/logout',
     },
     ACCOUNT: {
-        CONFIRM_EMAIL: '/account/confirm-email',
-        RESET_PASSWORD: '/account/reset-password',
+        CONFIRM_EMAIL: '/account/email-confirmation',
+        RESET_PASSWORD: '/account/password-reset',
         SEND_ACCOUNT_CONFIRMATION_MAIL: '/account/send-account-confirmation-mail',
-        SEND_RESET_PASSWORD_CONFIRMATION_MAIL: '/account/send-reset-password-confirmation-mail',
+        SEND_RESET_PASSWORD_CONFIRMATION_MAIL: '/account/password-reset',
+    },
+    EMAIL: {
+        ACCOUNT_CONFIRMATION: '/email/account-confirmation',
+        PASSWORD_RESET: '/email/password-reset',
     },
     USER: {
         AVATAR: '/user/avatar',
+        EMAIL: {
+            SEND_ACCOUNT_CONFIRMATION_MAIL: '/user/email/confirmation/send',
+        },
     },
 } as const;
--- src/routes/account/account.service.ts
+++ src/routes/account/account.service.ts
@@ -3,7 +3,7 @@ import { Constants } from '../../common/constants';
 import { hashString } from '../../common/helpers/hash-string';
 import { PrismaService } from '../../database/prisma.service';
 import { AccountConfirmationMail } from '../../services/email/mails/account-confirmation-mail';
-import { ResetPasswordConfirmationMail } from '../../services/email/mails/reset-password-confirmation-mail';
+import { PasswordResetMail } from '../../services/email/mails/password-reset-mail';
 import { SendConfirmationMailEvent } from '../../services/event/events/send-confirmation-mail-event';
 import { IEventService } from '../../services/event/interfaces/IEventService';
 import { ValidationService } from '../../services/validation/validation.service';
@@ -45,7 +45,7 @@ export class AccountService {
         this._eventService.handle(
             new SendConfirmationMailEvent({
                 id: user.id,
-                mail: new ResetPasswordConfirmationMail(user.email, {}),
+                mail: new PasswordResetMail(user.email, {}),
             }),
         );
     }

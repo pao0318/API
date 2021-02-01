8eab1c60850db223b85cd79f67c87aae0b650ac4
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-02-01 09:22:43 +0100

    Refactor config

--- src/common/config/index.ts
+++ src/common/config/index.ts
@@ -2,38 +2,62 @@ import { DotenvParseOutput, parse } from 'dotenv';
 import { join } from 'path';
 import { readFileSync } from 'fs';
 
-const variables = loadConfig() || process.env;
+class ConfigManager {
+    private readonly _environmentalConfig = process.env;
+    private readonly _fileConfig: DotenvParseOutput;
 
-export const Config = {
-    APP: {
-        MODE: process.env.NODE_ENV,
-        PREFIX: variables.APP_PREFIX || process.env.APP_PREFIX || '/',
-        PORT: parseInt(variables.PORT || process.env.PORT || variables.APP_PORT),
-    },
-    AUTH: {
-        ACCESS_TOKEN_SECRET: variables.AUTH_ACCESS_TOKEN_SECRET || process.env.AUTH_ACCESS_TOKEN_SECRET,
-    },
-    CLOUDINARY: {
-        CLOUD_NAME: variables.CLOUDINARY_CLOUD_NAME || process.env.CLOUDINARY_CLOUD_NAME,
-        API_KEY: variables.CLOUDINARY_API_KEY || process.env.CLOUDINARY_API_KEY,
-        API_SECRET: variables.CLOUDINARY_API_SECRET || process.env.CLOUDINARY_API_SECRET,
-    },
-    DATABASE: {
-        URL: variables.DATABASE_URL || process.env.DATABASE_URL,
-    },
-    MAIL: {
-        CLIENT_ID: variables.MAIL_CLIENT_ID || process.env.MAIL_CLIENT_ID,
-        CLIENT_SECRET: variables.MAIL_CLIENT_SECRET || process.env.MAIL_CLIENT_SECRET,
-        REFRESH_TOKEN: variables.MAIL_REFRESH_TOKEN || process.env.MAIL_REFRESH_TOKEN,
-        USER: variables.MAIL_USER || process.env.MAIL_USER,
-    },
-} as const;
+    constructor(fileConfig?: DotenvParseOutput) {
+        if (fileConfig) {
+            this._fileConfig = fileConfig;
+            return;
+        }
 
-function loadConfig(): DotenvParseOutput {
-    try {
-        const path = join(__dirname, '../../../env', `${process.env.NODE_ENV || 'development'}.env`);
-        return parse(readFileSync(path));
-    } catch (error) {
-        console.log('Existing config not found');
+        this._fileConfig = this._loadFileConfig();
+    }
+
+    public get config() {
+        return {
+            APP: {
+                MODE: this._getVariable('NODE_ENV'),
+                PREFIX: this._getVariable('APP_PREFIX') || '/',
+                PORT: parseInt(this._getVariable('PORT') || this._getVariable('APP_PORT')),
+            },
+            AUTH: {
+                ACCESS_TOKEN_SECRET: this._getVariable('ACCESS_TOKEN_SECRET'),
+            },
+            CLOUDINARY: {
+                CLOUD_NAME: this._getVariable('CLOUDINARY_CLOUD_NAME'),
+                API_KEY: this._getVariable('CLOUDINARY_API_KEY'),
+                API_SECRET: this._getVariable('CLOUDINARY_API_SECRET'),
+            },
+            DATABASE: {
+                URL: this._getVariable('DATABASE_URL'),
+            },
+            MAIL: {
+                CLIENT_ID: this._getVariable('MAIL_CLIENT_ID'),
+                CLIENT_SECRET: this._getVariable('MAIL_CLIENT_SECRET'),
+                REFRESH_TOKEN: this._getVariable('MAIL_REFRESH_TOKEN'),
+                USER: this._getVariable('MAIL_USER'),
+            },
+        } as const;
+    }
+
+    private _getVariable(name: string): string {
+        if (this._fileConfig[name.toString()]) {
+            return this._fileConfig[name.toString()];
+        }
+
+        return this._environmentalConfig[name.toString()];
+    }
+
+    private _loadFileConfig(): DotenvParseOutput {
+        try {
+            const path = join(__dirname, '../../../env', `${process.env.NODE_ENV}.env`);
+            return parse(readFileSync(path));
+        } catch (error) {
+            console.log('File config not found');
+        }
     }
 }
+
+export const Config = new ConfigManager().config;

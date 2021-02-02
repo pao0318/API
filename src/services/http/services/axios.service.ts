793f6ef8d238333418e40d8ab446a6feeab852d6
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-02-02 20:42:46 +0100

    Create services/http/services/axios.service.ts

--- src/services/http/services/axios.service.ts
+++ src/services/http/services/axios.service.ts
@@ -1,8 +1,10 @@
+import axios from 'axios';
 import { IHttpResponse } from '../interfaces/IHttpResponse';
 import { IHttpService } from '../interfaces/IHttpService';
 
 export class AxiosService implements IHttpService {
-    performGetRequest(url: string, headers?: Record<string, string>): Promise<IHttpResponse> {
-        throw new Error('Method not implemented.');
+    public async performGetRequest(url: string, headers?: Record<string, string>): Promise<IHttpResponse> {
+        const response = await axios.get(url, { headers });
+        return response;
     }
 }

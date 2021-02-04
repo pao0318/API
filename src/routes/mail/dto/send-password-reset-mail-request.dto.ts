be00602b8f6d8f31294fa151d66da80b2d62e626
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-02-04 14:48:37 +0100

    Add ApiProperty decorators to the existing dtos

--- src/routes/mail/dto/send-password-reset-mail-request.dto.ts
+++ src/routes/mail/dto/send-password-reset-mail-request.dto.ts
@@ -1,3 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class SendPasswordResetMailRequestDto implements Readonly<SendPasswordResetMailRequestDto> {
+    @ApiProperty({
+        minLength: 3,
+        maxLength: 64,
+    })
     public email: string;
 }
--- src/routes/user/dto/confirm-email-request.dto.ts
+++ src/routes/user/dto/confirm-email-request.dto.ts
@@ -1,4 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class ConfirmEmailRequestDto implements Readonly<ConfirmEmailRequestDto> {
+    @ApiProperty({
+        minLength: 3,
+        maxLength: 64,
+    })
     public email: string;
+
+    @ApiProperty({
+        minLength: 6,
+        maxLength: 6,
+    })
     public code: string;
 }

e91897d78b5f2c64215c3d434518230b2ce3da26
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-02-04 14:45:31 +0100

    Create common/loaders/swagger.ts

--- src/common/loaders/index.ts
+++ src/common/loaders/index.ts
@@ -4,6 +4,7 @@ import { loadConfig } from './config';
 import { loadExceptionListeners } from './exception-listeners';
 import { loadGlobalFilters } from './global-filters';
 import { loadMiddlewares } from './middlewares';
+import { loadSwagger } from './swagger';
 
 export const setupLoaders = (app: INestApplication): void => {
     loadExceptionListeners();
@@ -15,4 +16,6 @@ export const setupLoaders = (app: INestApplication): void => {
     loadMiddlewares(app);
 
     loadCloudinary();
+
+    loadSwagger(app);
 };
--- src/routes/auth/dto/login-request.dto.ts
+++ src/routes/auth/dto/login-request.dto.ts
@@ -1,4 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class LoginRequestDto implements Readonly<LoginRequestDto> {
+    @ApiProperty({
+        minLength: 3,
+        maxLength: 64,
+    })
     public email: string;
+
+    @ApiProperty({
+        minLength: 3,
+        maxLength: 64,
+    })
     public password: string;
 }

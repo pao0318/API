aa6f6ea859d46665e0915f89a9a4115ef6273149
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-02-06 15:52:35 +0100

    Create services/redis/redis.providers.ts

--- src/services/redis/redis.providers.ts
+++ src/services/redis/redis.providers.ts
@@ -1,1 +1,34 @@
-import { Redis } from 'ioredis';
+import { Provider } from '@nestjs/common';
+import RedisClient from 'ioredis';
+import { Config } from '../../common/config';
+import { Constants } from '../../common/constants';
+
+export const RedisProviders = [
+    {
+        useFactory: () => {
+            return new RedisClient({
+                host: Config.REDIS.HOST,
+                port: Config.REDIS.PORT
+            });
+        },
+        provide: Constants.DEPENDENCY.REDIS_CACHE_CLIENT
+    },
+    {
+        useFactory: () => {
+            return new RedisClient({
+                host: Config.REDIS.HOST,
+                port: Config.REDIS.PORT
+            });
+        },
+        provide: Constants.DEPENDENCY.REDIS_PUBLISHER_CLIENT
+    },
+    {
+        useFactory: () => {
+            return new RedisClient({
+                host: Config.REDIS.HOST,
+                port: Config.REDIS.PORT
+            });
+        },
+        provide: Constants.DEPENDENCY.REDIS_SUBSCRIBER_CLIENT
+    }
+] as Provider[];

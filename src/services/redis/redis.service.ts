0b3a59c808c76af66e962d4f6666eedcf5c2987d
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-02-07 16:20:38 +0100

    Create fromEvent method in services/redis/redis.service.ts

--- src/services/redis/redis.service.ts
+++ src/services/redis/redis.service.ts
@@ -1,12 +1,19 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { Observable, Observer } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 import { Constants } from '../../common/constants';
 import { IGetData } from './types/IGetData';
 import { ISetData } from './types/ISetData';
+import { ISubscribeMessage } from './types/ISubscribeMessage';
 import { RedisClient } from './types/RedisClient';
 
 @Injectable()
 export class RedisService {
-    constructor(@Inject(Constants.DEPENDENCY.REDIS_CACHE_CLIENT) private readonly _redisCacheClient: RedisClient) {}
+    constructor(
+        @Inject(Constants.DEPENDENCY.REDIS_CACHE_CLIENT) private readonly _redisCacheClient: RedisClient,
+        @Inject(Constants.DEPENDENCY.REDIS_PUBLISHER_CLIENT) private readonly _redisPublisherClient: RedisClient,
+        @Inject(Constants.DEPENDENCY.REDIS_SUBSCRIBER_CLIENT) private readonly _redisSubscriberClient: RedisClient
+    ) {}
 
     public async get(data: IGetData): Promise<string | null | Object> {
         const value = await this._redisCacheClient.get(data.key);
@@ -32,5 +39,16 @@ export class RedisService {
 
     public async clearCache(): Promise<void> {
         await this._redisCacheClient.flushall();
+    }
+
+    public fromEvent<T>(eventName: string): Observable<T> {
+        this._redisSubscriberClient.subscribe(eventName);
+
+        return Observable.create((observer: Observer<ISubscribeMessage>) =>
+            this._redisSubscriberClient.on('message', (channel, message) => observer.next({ message, channel }))
+        ).pipe(
+            filter(({ channel }) => channel === eventName),
+            map(({ message }) => JSON.parse(message))
+        );
     }
 }

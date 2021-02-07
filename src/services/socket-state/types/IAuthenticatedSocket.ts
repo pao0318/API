faa32af6978593754fee9436f6a9bedeffbd9138
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-02-07 20:12:03 +0100

    Create services/socket-state/socket-state.adapter.ts

--- src/services/socket-state/types/IAuthenticatedSocket.ts
+++ src/services/socket-state/types/IAuthenticatedSocket.ts
@@ -1,6 +1,6 @@
 import { Socket } from 'socket.io';
 import { IAccessTokenPayload } from '../../token/types/IAccessTokenPayload';
 
-export interface AuthenticatedSocket extends Socket {
+export interface IAuthenticatedSocket extends Socket {
     auth: IAccessTokenPayload;
 }

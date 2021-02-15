a6047cfff62337f8c55e39951c90fbb6f00ecbfd
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-02-15 07:53:06 +0100

    Create modules/user/dto/update-preference-body.dto.ts

--- src/modules/user/dto/update-preference-body.dto.ts
+++ src/modules/user/dto/update-preference-body.dto.ts
@@ -1,7 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Genre } from '@prisma/client';
+import { IsArray, IsDefined, IsEnum, IsString, MaxLength } from 'class-validator';
 
 export class UpdatePreferenceBodyDto implements Readonly<UpdatePreferenceBodyDto> {
+    @ApiProperty({ maxLength: 64 })
+    @IsString()
+    @IsDefined()
+    @MaxLength(64)
     public book: string;
+
+    @ApiProperty({ maxLength: 64 })
+    @IsString()
+    @IsDefined()
+    @MaxLength(64)
     public author: string;
+
+    @IsDefined()
+    @IsArray()
+    @IsEnum(Genre, { each: true })
     public genres: Genre[];
 }

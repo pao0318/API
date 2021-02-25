bdefb0bbd771cc625787aa6bf23ed4e4d452d994
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-02-25 17:46:01 +0100

    Refactor modules/book/__tests__/create-book-body.dto.test.ts

--- src/modules/book/__tests__/create-book-body.dto.test.ts
+++ src/modules/book/__tests__/create-book-body.dto.test.ts
@@ -1,21 +1,37 @@
-import { Genre } from '@prisma/client';
+import { Genre, Language } from '@prisma/client';
 import { validate } from 'class-validator';
 import { random } from 'faker';
 import { CreateBookBodyDto } from '../dto/create-book-body.dto';
 
-class TestCreateBookBodyDto extends CreateBookBodyDto {
-    constructor(data: { isbn?: string; genre?: Genre }) {
-        super();
+const generateDto = (input: Partial<CreateBookBodyDto>): CreateBookBodyDto => {
+    const data = { ...generateDtoData(), ...input };
+    const dto = new CreateBookBodyDto();
 
-        this.isbn = data.isbn;
-        this.genre = data.genre;
-    }
-}
+    dto.isbn = data.isbn;
+    dto.title = data.title;
+    dto.description = data.description;
+    dto.pages = data.pages;
+    dto.genre = data.genre;
+    dto.language = data.language;
+
+    return dto;
+};
+
+const generateDtoData = () => {
+    return {
+        isbn: '7853079263849',
+        title: random.alphaNumeric(10),
+        description: random.alphaNumeric(10),
+        pages: random.number({ min: 1, max: 3000 }),
+        genre: random.arrayElement(Object.values(Genre)),
+        language: random.arrayElement(Object.values(Language))
+    };
+};
 
 describe('Create Book Body Dto', () => {
     describe('When isbn does not exist', () => {
         it('Should return a validation error', async () => {
-            const body = new TestCreateBookBodyDto({ genre: random.arrayElement(Object.values(Genre)) });
+            const body = generateDto({ isbn: undefined });
 
             const errors = await validate(body);
 
@@ -25,8 +41,8 @@ describe('Create Book Body Dto', () => {
 
     describe('When isbn is not a string', () => {
         it('Should return a validation error', async () => {
-            //@ts-expect-error
-            const body = new TestCreateBookBodyDto({ isbn: false, genre: random.arrayElement(Object.values(Genre)) });
+            /* @ts-expect-error */
+            const body = generateDto({ isbn: random.number() });
 
             const errors = await validate(body);
 
@@ -36,7 +52,7 @@ describe('Create Book Body Dto', () => {
 
     describe('When isbn is not a numeric string', () => {
         it('Should return a validation error', async () => {
-            const body = new TestCreateBookBodyDto({ isbn: random.word(), genre: random.arrayElement(Object.values(Genre)) });
+            const body = generateDto({ isbn: random.word() });
 
             const errors = await validate(body);
 
@@ -46,7 +62,7 @@ describe('Create Book Body Dto', () => {
 
     describe('When isbn has less than 13 characters', () => {
         it('Should return a validation error', async () => {
-            const body = new TestCreateBookBodyDto({ isbn: '75101086', genre: random.arrayElement(Object.values(Genre)) });
+            const body = generateDto({ isbn: '75101086' });
 
             const errors = await validate(body);
 
@@ -56,7 +72,140 @@ describe('Create Book Body Dto', () => {
 
     describe('When isbn has more than 13 characters', () => {
         it('Should return a validation error', async () => {
-            const body = new TestCreateBookBodyDto({ isbn: '751010865981710686161', genre: random.arrayElement(Object.values(Genre)) });
+            const body = generateDto({ isbn: '751010865981710686161' });
+
+            const errors = await validate(body);
+
+            expect(errors).toHaveLength(1);
+        });
+    });
+
+    describe('When title does not exist', () => {
+        it('Should return a validation error', async () => {
+            const body = generateDto({ title: undefined });
+
+            const errors = await validate(body);
+
+            expect(errors).toHaveLength(1);
+        });
+    });
+
+    describe('When title is not a string', () => {
+        it('Should return a validation error', async () => {
+            /* @ts-expect-error */
+            const body = generateDto({ title: random.number() });
+
+            const errors = await validate(body);
+
+            expect(errors).toHaveLength(1);
+        });
+    });
+
+    describe('When title has less than 1 character', () => {
+        it('Should return a validation error', async () => {
+            const body = generateDto({ title: '' });
+
+            const errors = await validate(body);
+
+            expect(errors).toHaveLength(1);
+        });
+    });
+
+    describe('When title has more than 100 characters', () => {
+        it('Should return a validation error', async () => {
+            const body = generateDto({ title: random.alphaNumeric(150) });
+
+            const errors = await validate(body);
+
+            expect(errors).toHaveLength(1);
+        });
+    });
+
+    describe('When description does not exist', () => {
+        it('Should return a validation error', async () => {
+            const body = generateDto({ description: undefined });
+
+            const errors = await validate(body);
+
+            expect(errors).toHaveLength(1);
+        });
+    });
+
+    describe('When description is not a string', () => {
+        it('Should return a validation error', async () => {
+            /* @ts-expect-error */
+            const body = generateDto({ description: random.number() });
+
+            const errors = await validate(body);
+
+            expect(errors).toHaveLength(1);
+        });
+    });
+
+    describe('When description has less than 1 character', () => {
+        it('Should return a validation error', async () => {
+            const body = generateDto({ description: '' });
+
+            const errors = await validate(body);
+
+            expect(errors).toHaveLength(1);
+        });
+    });
+
+    describe('When description has more than 1000 characters', () => {
+        it('Should return a validation error', async () => {
+            const body = generateDto({ description: random.alphaNumeric(1050) });
+
+            const errors = await validate(body);
+
+            expect(errors).toHaveLength(1);
+        });
+    });
+
+    describe('When pages does not exist', () => {
+        it('Should return a validation error', async () => {
+            const body = generateDto({ pages: undefined });
+
+            const errors = await validate(body);
+
+            expect(errors).toHaveLength(1);
+        });
+    });
+
+    describe('When pages is not a number', () => {
+        it('Should return a validation error', async () => {
+            /* @ts-expect-error */
+            const body = generateDto({ pages: random.alphaNumeric(10) });
+
+            const errors = await validate(body);
+
+            expect(errors).toHaveLength(1);
+        });
+    });
+
+    describe('When pages is smaller than 1', () => {
+        it('Should return a validation error', async () => {
+            const body = generateDto({ pages: random.number(0) });
+
+            const errors = await validate(body);
+
+            expect(errors).toHaveLength(1);
+        });
+    });
+
+    describe('When pages is bigger than 3000', () => {
+        it('Should return a validation error', async () => {
+            const body = generateDto({ pages: random.number({ min: 3050 }) });
+
+            const errors = await validate(body);
+
+            expect(errors).toHaveLength(1);
+        });
+    });
+
+    describe('When genre does not exist', () => {
+        it('Should return a validation error', async () => {
+            const body = generateDto({ genre: undefined });
 
             const errors = await validate(body);
 
@@ -66,8 +215,8 @@ describe('Create Book Body Dto', () => {
 
     describe('When genre is not a string', () => {
         it('Should return a validation error', async () => {
-            //@ts-expect-error
-            const body = new TestCreateBookBodyDto({ isbn: '7853079263849', genre: [false] });
+            /* @ts-expect-error */
+            const body = generateDto({ genre: random.number() });
 
             const errors = await validate(body);
 
@@ -77,8 +226,40 @@ describe('Create Book Body Dto', () => {
 
     describe('When genre is not a valid enum', () => {
         it('Should return a validation error', async () => {
-            //@ts-expect-error
-            const body = new TestCreateBookBodyDto({ isbn: '7853079263849', genre: random.word() });
+            /* @ts-expect-error */
+            const body = generateDto({ genre: random.word() });
+
+            const errors = await validate(body);
+
+            expect(errors).toHaveLength(1);
+        });
+    });
+
+    describe('When language does not exist', () => {
+        it('Should return a validation error', async () => {
+            const body = generateDto({ language: undefined });
+
+            const errors = await validate(body);
+
+            expect(errors).toHaveLength(1);
+        });
+    });
+
+    describe('When language is not a string', () => {
+        it('Should return a validation error', async () => {
+            /* @ts-expect-error */
+            const body = generateDto({ language: random.number() });
+
+            const errors = await validate(body);
+
+            expect(errors).toHaveLength(1);
+        });
+    });
+
+    describe('When language is not a valid enum', () => {
+        it('Should return a validation error', async () => {
+            /* @ts-expect-error */
+            const body = generateDto({ language: random.word() });
 
             const errors = await validate(body);
 
@@ -88,7 +269,7 @@ describe('Create Book Body Dto', () => {
 
     describe('When the data is valid', () => {
         it('Should not return any validation errors', async () => {
-            const body = new TestCreateBookBodyDto({ isbn: '7853079263849', genre: random.arrayElement(Object.values(Genre)) });
+            const body = generateDto({});
 
             const errors = await validate(body);
 

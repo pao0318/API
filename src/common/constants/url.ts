5f2c897dbd748ec19c33c3128c1d65b974379103
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-02-27 12:45:19 +0100

    Remove external quotes API

--- src/common/constants/url.ts
+++ src/common/constants/url.ts
@@ -1,4 +1,3 @@
 export const Url = {
-    GOOGLE_BOOKS_API: 'https://www.googleapis.com/books/v1/volumes',
-    QUOTES_API: 'https://type.fit/api/quotes'
+    GOOGLE_BOOKS_API: 'https://www.googleapis.com/books/v1/volumes'
 } as const;
